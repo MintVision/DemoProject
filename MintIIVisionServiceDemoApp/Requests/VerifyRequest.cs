14c8deb16edcd394fabaac3e447a9c4c0ec94e52
PhinaDavis <85176034+PhinaDavis@users.noreply.github.com>
2022-11-04 14:12:00 +0200

    Removed obselete properties

--- MintIIVisionServiceDemoApp/Requests/VerifyRequest.cs
+++ MintIIVisionServiceDemoApp/Requests/VerifyRequest.cs
@@ -11,8 +11,6 @@ namespace MintIIVisionServiceDemoApp.Requests
         public string PhoneNumber { get; set; }
         public string RequestedByUserId { get; set; }
         public string RequestedByName { get; set; }
-        public string AdditionalQuestion { get; set; }
-        public string AdditionalAnswer { get; set; }
         public bool ScanIdDocument { get; set; } 
         public bool AskThirdPartyQuestion { get; set; }
         public string ConfirmAction { get; set; }
